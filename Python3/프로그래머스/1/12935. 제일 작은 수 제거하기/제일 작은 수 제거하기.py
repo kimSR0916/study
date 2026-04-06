1283bd7870a27bd942770a7799fdd4d537432337
kimSR0916 <143482628+kimSR0916@users.noreply.github.com>
2026-04-06 22:33:06 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 1.05 ms, Memory: 14.7 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
@@ -0,0 +1,12 @@
+def solution(arr):
+    answer = []
+    
+    if (len(arr) == 1):
+        answer.append(-1)
+    else:
+        m = min(arr)
+        arr.remove(m)
+        answer = arr
+    
+    
+    return answer
