3494f80ea91ff3437f7920d37a6c515182764fb6
kimSR0916 <143482628+kimSR0916@users.noreply.github.com>
2026-04-07 17:51:56 +0900

    Add second solution for matrix addition
    Added a second solution for matrix addition that modifies the first matrix in place.

--- Python3/프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ Python3/프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -8,3 +8,13 @@ def solution(arr1, arr2):
         answer.append(row)
         
     return answer
+
+#2차풀이
+def solution(arr1, arr2):
+    answer = arr1
+
+    for i in range (len(arr2)):
+        for j in range (len(arr2[i])):
+            answer[i][j] += arr2[i][j]
+        
+    return answer
